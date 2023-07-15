2c52e8820fbbc21295498920293a012336a996ff
HanZawNyein <hanzawnyein@192.168.1.5>
2023-07-15 21:02:17 +0630

    [add] agency

--- addons/travel/__manifest__.py
+++ addons/travel/__manifest__.py
@@ -1,11 +1,15 @@
 {
-    "name":"Travelling",
-    "data":[
+    "name": "Travelling",
+    "data": [
         # security
         "security/ir.model.access.csv",
 
         # views
         "views/township.xml",
         "views/travel_gate.xml",
+        "views/travel_agency.xml",
+
+        # menus
+        'views/menus.xml'
     ]
 }
