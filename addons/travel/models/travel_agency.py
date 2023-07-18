f4ccd394a29a85195d0a77e0f8915e4588b595a4
HanZawNyein <hanzawnyein@192.168.1.3>
2023-07-19 00:18:33 +0630

    [add] database level -constrains

--- addons/travel/models/travel_agency.py
+++ addons/travel/models/travel_agency.py
@@ -11,14 +11,18 @@ class TravelAgency(models.Model):
     reference = fields.Char(tracking=True, default=lambda self: _("New"), copy=False)
     license_number = fields.Char(tracking=True)
 
+    _sql_constraints = [
+        ('license_number_unique', 'unique(license_number)', 'License Number be unique !')
+    ]
+
+    @api.constrains('name')
+    def _check_name(self):
+        if self.name in self.search([('id', '!=', self.id)]).mapped('name'):
+            raise UserError(_("Agency Name is already exists."))
+
     @api.model
     def create(self, values):
         if values.get('reference', _('New')) == _('New'):
             values['reference'] = self.env['ir.sequence'].next_by_code('travel.agency') or _('New')
         # Add code here
         return super(TravelAgency, self).create(values)
-
-    @api.constrains('name')
-    def _check_name(self):
-        if self.name in self.search(['id', '!=', self.id]).mapped('name'):
-            raise UserError(_("Agency Name is already exists."))
