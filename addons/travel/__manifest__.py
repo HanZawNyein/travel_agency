74d1159fd8b8442c21b30b2893511db6592ec0fb
HanZawNyein <hanzawnyein@192.168.1.4>
2023-07-29 23:15:36 +0630

    [add] wizard

--- addons/travel/__manifest__.py
+++ addons/travel/__manifest__.py
@@ -16,5 +16,8 @@
 
         # menus
         'views/menus.xml',
+
+        # wizard
+        "wizard/change_driver_wizard.xml",
     ]
 }
---
+++ addons/travel/wizard/change_driver_wizard.py
@@ -0,0 +1,20 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+
+class ChangeDriverWizard(models.TransientModel):
+    _name = 'change.driver.wizard'
+    _description = 'Change Driver Wizard'
+
+    partner_id = fields.Many2one('res.partner', required=True)
+    join_date = fields.Date(default=lambda self: fields.Date.today())
+
+    def change_driver(self):
+        context = self.env.context
+        active_model = context.get('active_model')  # travel.car
+        active_id = context.get('active_id')  # 1
+        travel_card_id = self.env[active_model].browse(active_id)
+        if self.partner_id == travel_card_id.partner_id:
+            raise UserError(_("Change Driver should not be same."))
+        travel_card_id.partner_id = self.partner_id
+        travel_card_id.join_date = self.join_date
