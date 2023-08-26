aff61892dfc3860c2f2bd2c8118f237a7b6b3316
HanZawNyein <hanzawnyein@Hans-MacBook-Pro-2.local>
2023-08-26 22:16:14 +0630

    [add] line create from parent model

--- addons/travel/models/travel_booking.py
+++ addons/travel/models/travel_booking.py
@@ -1,4 +1,5 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 
 class TravelBooking(models.Model):
@@ -19,8 +20,42 @@ class TravelBooking(models.Model):
     currency_id = fields.Many2one('res.currency', related="company_id.currency_id")
     total_amount = fields.Monetary(currency_field="currency_id", compute="_compute_total_amount")
     per_seat = fields.Monetary(related="transportation_route_id.per_seat")
+    travel_booking_line_ids = fields.One2many('travel.booking.line', 'travel_booking_id')
 
     @api.depends('partner_ids')
     def _compute_total_amount(self):
         for rec in self:
             rec.total_amount = len(rec.partner_ids) * rec.per_seat
+
+    @api.model
+    def create(self, values):
+        # Add code here
+        if values.get('transportation_route_id'):
+            transportation_route_id = self.transportation_route_id.browse(values.get('transportation_route_id'))
+            already_seats = set(transportation_route_id.transportation_route_line_ids.mapped('seat_number'))
+            all_seats = set(range(1, transportation_route_id.seat + 1))
+            available_seats = all_seats - already_seats
+            if available_seats:
+                values["travel_booking_line_ids"] = [
+                    (0, 0, {"travel_booking_id": self.id, "seat_number": seat})
+                    for seat in available_seats
+                ]
+        return super(TravelBooking, self).create(values)
+
+    def action_confirm(self):
+        booking = self.travel_booking_line_ids.filtered(
+            lambda booking_line_id: booking_line_id.booking == True)
+        if not len(self.partner_ids) == len(booking):
+            raise UserError(_("Matching Failed."))
+        self.state = "confirm"
+        self.travel_booking_line_ids.filtered(
+            lambda booking_line_id: booking_line_id.booking == False).unlink()
+
+
+class TravelBookingLine(models.Model):
+    _name = 'travel.booking.line'
+    _description = 'TravelBookingLine'
+
+    travel_booking_id = fields.Many2one('travel.booking')
+    seat_number = fields.Integer()
+    booking = fields.Boolean(default=False)
